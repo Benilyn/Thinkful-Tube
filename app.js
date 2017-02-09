f8b4609f4db26228ac7e428161089283dc623837
Benilyn <benilyn.gonzales@gmail.com>
2017-02-08 23:31:10 -0800

    added getAPI and displaySearch function

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-/*
- 
-
- -- get API
-$.getJSON(searchURL, -, -);
-
-
-*/
-
-
-
-
 
 
 var searchURL = 'https://www.googleapis.com/youtube/v3/search';
 
-$(document).ready(function(){
-	$('form').submit(function(){
-		event.preventDefault();
-		var toSearch = $(this).find('.input-search').val();
-	});
+function getApi(toSearch, callback){
+			var query = {
+				q: toSearch,
+				key: 'AIzaSyC9nQIHsILpfiajJV7rBo5IKJ7hVh_CLlA',
+				part: 'snippet'
+			};
+			$.getJSON(searchURL, query, callback);
+		}
 
+function displaySearch(data) {
+			var result = '';
+			if (data.items) {
+				data.items.forEach(function(searchResult){
+				result += '<p>' + searchResult.snippet.title + '</p>';
+
+				});
+			}
+			else {
+				result += '<p>Search not Found</p>';
+			}
+			$('.js-search-results').html(result);	
+		}
+
+$(document).ready(function(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		var toSearch = $(this).find('.input-search').val();	
+		getApi(toSearch, displaySearch);
+		
+	});
 });
+
+
+/*
+$.getJSON('http://example.com/search', {query: 'food', format: 'json'}function(response){
+	console.log(response.data)
+})
+*/
+
